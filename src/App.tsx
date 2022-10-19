35730913af273ddfebced84c3b7fbed76b09c7cd
Israel Alves <devisrael98@gmail.com>
2022-10-19 13:59:37 -0300

    feat: error commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { MenuLateral } from "./shared/components";
+import { DrawerProvider } from "./shared/contexts";
 import { AppThemeProvider } from "./shared/contexts/ThemeContext";
 
 export const App = () => {
   return (
     <AppThemeProvider>
+    <DrawerProvider>
+
       <BrowserRouter>
+
+      <MenuLateral>
         <AppRoutes/>
+      </MenuLateral>
+      
       </BrowserRouter>
+
+    </DrawerProvider>  
     </AppThemeProvider>  
   );
 }
