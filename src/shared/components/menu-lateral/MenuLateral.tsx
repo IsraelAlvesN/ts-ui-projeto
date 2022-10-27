429be086a5bcecf0566c75e82d6726a75aeef6be
Israel Alves <devisrael98@gmail.com>
2022-10-27 09:44:46 -0300

    feat: Criado dashboard e menu lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,16 +1,47 @@
 import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from "@mui/material"
 import React, { ReactNode } from "react"
-import { useDrawerContext } from "../../contexts";
+import { useMatch, useNavigate, useResolvedPath } from "react-router-dom";
+import { useAppThemeContext, useDrawerContext } from "../../contexts";
+
+interface IListItemLinkProps{
+    to: string;
+    icon: string;
+    label: string;
+    onClick: (() => void) | undefined;
+}
 
 interface IMenuLateral{
     children: ReactNode;
 }
 
+const ListItemLink:React.FC<IListItemLinkProps> = ({to, icon, label, onClick}) => {
+    const navigate = useNavigate()
+
+    const resolvedPath = useResolvedPath(to)
+    const match = useMatch({path: resolvedPath.pathname, end: false})
+
+    const handleClick = () => {
+        navigate(to);
+        onClick?.();
+    }
+
+    return(
+        <ListItemButton selected={!!match} onClick={handleClick}>
+            <ListItemIcon>
+                <Icon>{icon}</Icon>
+            </ListItemIcon>
+            <ListItemText primary={label} />
+        </ListItemButton>
+    )
+}
+
 export const MenuLateral: React.FC<IMenuLateral> = ({children}) => {
     const theme = useTheme()
     const smDown = useMediaQuery(theme.breakpoints.down('sm'))
+    
+    const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext()
 
-    const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext()
+    const {toggleTheme} = useAppThemeContext()
 
     return(
         <>
@@ -21,16 +52,31 @@ export const MenuLateral: React.FC<IMenuLateral> = ({children}) => {
                     </Box>
                 </Box>
                 <Divider/>
+
                 <Box flex={1}>
-                <List component="nav">
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <Icon>home</Icon>
-                        </ListItemIcon>
-                        <ListItemText primary='Página Inicial' />
-                    </ListItemButton>
-                </List>
+                    <List component="nav">
+                        {drawerOptions.map( drawerOption => (
+                            <ListItemLink
+                                key={drawerOption.path}
+                                icon={drawerOption.icon}
+                                to={drawerOption.path}
+                                label={drawerOption.label}
+                                onClick={smDown ? toggleDrawerOpen : undefined}
+                            />
+                        ))}
+                    </List>
                 </Box>
+                <Box>
+                    <List component="nav">
+                    <ListItemButton onClick={toggleTheme}>
+                        <ListItemIcon>
+                            <Icon>dark_mode</Icon>
+                        </ListItemIcon>
+                        <ListItemText primary='Alternar tema' />
+                    </ListItemButton>
+                    </List>
+                </Box>
+
             </Drawer>
             <Box height='100vh' marginLeft={smDown ? 0 : theme.spacing(28)}>
                 {children}
@@ -38,5 +84,4 @@ export const MenuLateral: React.FC<IMenuLateral> = ({children}) => {
         </>
     )
 }
-//10
 
--- src/shared/contexts/DrawerContext.tsx
+++ src/shared/contexts/DrawerContext.tsx
@@ -1,8 +1,17 @@
 import React, { createContext, ReactNode, useCallback, useContext, useState } from "react";
 
+interface IDrawerOption{
+    // to: string;
+    path: string;
+    label: string;
+    icon: string;
+}
+
 interface IDrawerContextData {
     isDrawerOpen: boolean;
     toggleDrawerOpen: () => void;
+    drawerOptions : IDrawerOption[];
+    setDrawerOptions: (newDrawerOptions: IDrawerOption[]) => void;
 }
 
 interface IAppThemeProvider{
@@ -17,13 +26,17 @@ export const useDrawerContext = () => {
 
 export const DrawerProvider: React.FC<IAppThemeProvider> = ({children}) => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+    const [drawerOptions, setDrawerOptions] = useState<IDrawerOption[]>([])
 
     const toggleDrawerOpen = useCallback(() => {
         setIsDrawerOpen(oldDrawerOpen => !oldDrawerOpen)
     }, [])
+    const handleSetDrawerOptions = useCallback((newDrawerOptions: IDrawerOption[]) => {
+        setDrawerOptions(newDrawerOptions)
+    }, [])
 
     return(
-        <DrawerContext.Provider value={{isDrawerOpen, toggleDrawerOpen}}>
+        <DrawerContext.Provider value={{isDrawerOpen, toggleDrawerOpen, drawerOptions, setDrawerOptions: handleSetDrawerOptions}}>
             {children}
         </DrawerContext.Provider>
     )
